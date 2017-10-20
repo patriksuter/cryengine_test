026c06645cb1e5749e8dd6282d9da774e7280fbc
Actionson <32912184+Actionson@users.noreply.github.com>
2017-10-20 02:20:40 +0200

    Re

--- CRYENGINE Projects/user/sandbox/python/autocomplete/sandbox/entity.py
+++
@@ -1,8 +0,0 @@
-def open_archetype(arg1):
-    """
-    Open archetype in a Entity Archetype editor
-    
-    :param str arg1:
-    """
-    pass
-
--- Hyphearia/user/sandbox/python/autocomplete/sandbox/particle.py
+++
@@ -1,8 +0,0 @@
-def show_effect(arg1):
-    """
-    Show a specific particle effect in the Particle Editor.
-    
-    :param str arg1:
-    """
-    pass
-
